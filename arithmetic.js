befea363a26c300c5389e0b3c74a66fb7edbcc32
Murugan20051811 <saranmurugan69@gmail.com>
2025-02-05 00:12:08 -0800

    Add files via upload

---
+++ arithmetic.js
@@ -0,0 +1,40 @@
+let a=100;
+let b=20;
+c=a+b;
+// console.log(c);
+// d=c*b;
+// e=d-a;
+// f=e/c;
+// g=f%d;
+// h=b**2;
+// console.log(d);
+// console.log(e);
+// console.log(g);
+// console.log(h);
+// 
+// console.log("Bio Data");
+ 
+// console.log("name: Murugan M");
+// console.log("date of birth: 18/11/2005");
+// console.log("Gender: male");
+// console.log("marital status: Not married");
+// console.log("Nationality: Indian");
+// console.log("religion: Hindu");
+// console.log("Degree: B.Tect");
+// console.log("University: Anna University");
+// console.log("Address: 35, Palla Street, Kondamur, vanur-tk,villupuram-dt");
+
+// function myfun(){
+//     console.log("Hello java");
+// }
+// myfun();
+
+// function myfun(a,b){
+//     console.log(a+b);
+// }
+// myfun(10,20);
+
+function myfun(name){
+    console.log("welcome",name);
+}
+myfun(sachin);
